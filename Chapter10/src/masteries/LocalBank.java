b2767806aa5229b7efe74ef25b2a1652c82ee0fa
27355005 <27355005@L815WD220003.edu.cbe.ab.ca>
2024-09-24 15:24:34 -0600

    9/24/2024, 3:24

--- Chapter10/src/masteries/LocalBank.java
+++ Chapter10/src/masteries/LocalBank.java
@@ -11,11 +11,19 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class LocalBank {
 
 	private JFrame frame;
-	private JTextField textField;
+	private JTextField acctNum;
+	private JTextField amt;
+	private JTextField fname;
+	private JTextField lname;
+	private JTextField bgBalance;
 
 	/**
 	 * Launch the application.
@@ -45,24 +53,88 @@ public class LocalBank {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 587, 441);
+		frame.setBounds(100, 100, 602, 468);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(158, 34, 86, 20);
-		panel.add(textField);
-		textField.setColumns(10);
+		JComboBox BankAct = new JComboBox();
+		BankAct.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		BankAct.setModel(new DefaultComboBoxModel(new String[] {"Select an Item"}));
+		BankAct.setBounds(57, 41, 483, 22);
+		panel.add(BankAct);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(61, 92, 86, 22);
-		panel.add(comboBox);
+		acctNum = new JTextField();
+		acctNum.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				acctNum.setText("");
+			}
+		});
+		acctNum.setText("Account number ");
+		acctNum.setBounds(57, 98, 483, 22);
+		panel.add(acctNum);
+		acctNum.setColumns(10);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(182, 141, 89, 23);
-		panel.add(btnNewButton);
+		amt = new JTextField();
+		amt.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				amt.setText("");
+
+			}
+		});
+		amt.setText("Amount");
+		amt.setColumns(10);
+		amt.setBounds(57, 144, 483, 22);
+		panel.add(amt);
+		
+		fname = new JTextField();
+		fname.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				fname.setText("");
+			}
+		});
+		fname.setText("First name");
+		fname.setColumns(10);
+		fname.setBounds(57, 192, 483, 22);
+		panel.add(fname);
+		
+		lname = new JTextField();
+		lname.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				lname.setText("");
+			}
+		});
+		lname.setText("Last name");
+		lname.setColumns(10);
+		lname.setBounds(57, 236, 483, 22);
+		panel.add(lname);
+		
+		bgBalance = new JTextField();
+		bgBalance.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				bgBalance.setText("");
+			}
+		});
+		bgBalance.setText("Begining balance");
+		bgBalance.setColumns(10);
+		bgBalance.setBounds(57, 290, 483, 22);
+		panel.add(bgBalance);
+		
+		JLabel actinfo = new JLabel("");
+		actinfo.setBounds(163, 333, 377, 27);
+		panel.add(actinfo);
+		
+		JButton ahhadlkjdf = new JButton("Process transaction");
+		ahhadlkjdf.setBounds(24, 382, 127, 22);
+		panel.add(ahhadlkjdf);
+		
+		
 	}
 }
